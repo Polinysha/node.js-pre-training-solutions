3e0a30151661eea738c269e92d52c1945c40ab82
Polinysha <datkopolina43@gmail.com>
2025-11-28 14:39:55 +0300

    Update todo-crud.ts

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,30 @@
+//4
 import { Todo } from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+    return [...state, todo];
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+    const todoExists = state.some(todo => todo.id === id);
+    if (!todoExists) {
+        throw new Error(`Todo with id ${id} not found`);
+    }
+    
+    return state.map(todo =>
+        todo.id === id ? { ...todo, ...update } : todo
+    );
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+    const todoExists = state.some(todo => todo.id === id);
+    if (!todoExists) {
+        throw new Error(`Todo with id ${id} not found`);
+    }
+    
+    return state.filter(todo => todo.id !== id);
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+    return state.find(todo => todo.id === id);
 }
