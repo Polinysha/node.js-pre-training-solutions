f2a3ff6104508fabf391b6ccef5312bd0448e27d
Polinysha <datkopolina43@gmail.com>
2025-11-28 14:43:07 +0300

    Update todo-api.ts

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,22 +1,61 @@
-import { InMemoryRepository } from './repository';
-import { Todo, NewTodo } from './types';
+//6
+import { Todo, NewTodo, TodoStatus } from './types';
+
+class TodoNotFoundError extends Error {
+    constructor(id: number) {
+        super(`Todo with id ${id} not found`);
+        this.name = 'TodoNotFoundError';
+    }
+}
 
 export class TodoApi {
-  private repo = new InMemoryRepository<Todo>();
+    private todos: Todo[] = [];
+    private nextId = 1;
 
-  async getAll(): Promise<Todo[]> {
-    throw new Error('getAll: not implemented');
-  }
+    private delay(ms: number): Promise<void> {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-  async add(newTodo: NewTodo): Promise<Todo> {
-    throw new Error('add: not implemented');
-  }
+    async getAll(): Promise<Todo[]> {
+        await this.delay(300 + Math.random() * 300);
+        return [...this.todos];
+    }
 
-  async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    throw new Error('update: not implemented');
-  }
+    async add(newTodo: NewTodo): Promise<Todo> {
+        await this.delay(300 + Math.random() * 300);
+        
+        const todo: Todo = {
+            id: this.nextId++,
+            title: newTodo.title,
+            description: newTodo.description,
+            status: newTodo.status || TodoStatus.PENDING,
+            createdAt: new Date()
+        };
 
-  async remove(id: number): Promise<void> {
-    throw new Error('remove: not implemented');
-  }
+        this.todos.push(todo);
+        return todo;
+    }
+
+    async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
+        await this.delay(300 + Math.random() * 300);
+        
+        const index = this.todos.findIndex(todo => todo.id === id);
+        if (index === -1) {
+            throw new TodoNotFoundError(id);
+        }
+
+        this.todos[index] = { ...this.todos[index], ...update };
+        return this.todos[index];
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.delay(300 + Math.random() * 300);
+        
+        const index = this.todos.findIndex(todo => todo.id === id);
+        if (index === -1) {
+            throw new TodoNotFoundError(id);
+        }
+
+        this.todos.splice(index, 1);
+    }
 }
