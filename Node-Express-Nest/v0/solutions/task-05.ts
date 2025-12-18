331c73a11b482ada0062507a69656e480935990b
Polinysha <datkopolina43@gmail.com>
2025-12-18 19:37:06 +0300

    Update task-05.ts

--- Node-Express-Nest/v0/solutions/task-05.ts
+++ Node-Express-Nest/v0/solutions/task-05.ts
@@ -1,11 +1,10 @@
-// NestJS Controller for /todos
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('todos')
 export class TodosController {
   @Get()
   getTodos() {
-    // ТОЧНО как в примере задания: возвращает массив todos
+
     return [
       { id: 1, title: 'Buy milk', completed: false }
     ];
