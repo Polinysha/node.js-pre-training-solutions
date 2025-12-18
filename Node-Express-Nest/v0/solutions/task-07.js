db8fb7e313b73a4c94b427f636379edd27a5f670
Polinysha <datkopolina43@gmail.com>
2025-12-18 19:37:46 +0300

    Update task-07.js

--- Node-Express-Nest/v0/solutions/task-07.js
+++ Node-Express-Nest/v0/solutions/task-07.js
@@ -1,6 +1,5 @@
-// Express.js error handler middleware for ToDo API
 function errorHandlerMiddleware(err, req, res, next) {
-  // ТОЧНО как в задании: returns { error: <message> }
+
   res.status(err.status || 500).json({
     error: err.message || 'Internal Server Error'
   });
