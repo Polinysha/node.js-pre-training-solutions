ff87c14ef8134ca769f61e784affda39adf1964e
Polinysha <datkopolina43@gmail.com>
2025-12-18 19:36:27 +0300

    Update task-03.js

--- Node-Express-Nest/v0/solutions/task-03.js
+++ Node-Express-Nest/v0/solutions/task-03.js
@@ -1,6 +1,4 @@
-// Express.js logging middleware for ToDo API
 function loggingMiddleware(req, res, next) {
-  // ТОЧНО как в примере задания: logs "GET /todos"
   console.log(`${req.method} ${req.url}`);
   next();
 }
