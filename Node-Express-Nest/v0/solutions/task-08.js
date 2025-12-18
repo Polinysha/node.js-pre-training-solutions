601aa34b4d6f813b084982d6f6f2add1936cbc18
Polinysha <datkopolina43@gmail.com>
2025-12-18 19:38:00 +0300

    Update task-08.js

--- Node-Express-Nest/v0/solutions/task-08.js
+++ Node-Express-Nest/v0/solutions/task-08.js
@@ -1,9 +1,8 @@
-// Express.js static files serving for ToDo frontend
 const express = require('express');
 const path = require('path');
 
 function setupStaticFiles(app) {
-  // ТОЧНО как в задании: Serve static files from a `public` directory at `/static`
+
   const publicDir = path.join(__dirname, 'public');
   app.use('/static', express.static(publicDir));
 }
